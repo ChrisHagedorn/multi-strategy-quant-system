ae91d4699f67a57410dde3b3f5bafd49779166c0
Chris Hagedorn <1844858+ChrisHagedorn@users.noreply.github.com>
2023-08-05 16:43:32 +0800

    pull s&p 500 data and generate xlsx

--- main.py
+++ main.py
@@ -2,15 +2,44 @@ import pandas as pd
 import requests
 import yfinance as yf
 from bs4 import BeautifulSoup
+import xlsxwriter
 
 def get_sp500_instruments():
     res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
     soup = BeautifulSoup(res.content,'lxml')
     table = soup.find_all('table')[0] 
     df = pd.read_html(str(table))
-    print(df[0])
-    return list(df["Symbol"])
+    return list(df[0]["Symbol"])
 
-tickers = get_sp500_instruments()
-print(tickers)
-get_sp500_instruments()
+
+def get_sp500_df():
+    symbols = get_sp500_instruments()
+    symbols = symbols[:30] 
+    ohlcvs = {}
+    for symbol in symbols:
+        symbol_df = yf.Ticker(symbol).history(period="10y")
+        ohlcvs[symbol] = symbol_df[["Open", "High", "Low", "Close", "Volume"]].rename(
+            columns={
+                "Open": "open",
+                "High": "high",
+                "Low": "low",
+                "Close": "close",
+                "Volume": "volume"
+            }
+        )
+        print(symbol)
+        print(ohlcvs[symbol])
+    
+    df = pd.DataFrame(index=ohlcvs["GOOGL"].index)
+    df.index.name = "date"
+    instruments = list(ohlcvs.keys())
+    for inst in instruments:
+        inst_df = ohlcvs[inst]
+        columns = list(map(lambda x: "{} {}".format(inst, x), inst_df.columns))
+        df[columns] = inst_df
+
+    return df, instruments
+
+df, instruments = get_sp500_df()
+
+df.to_excel("./data/sp500_data.xlsx", engine='xlsxwriter')  
