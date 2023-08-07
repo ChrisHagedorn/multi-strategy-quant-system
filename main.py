10f1412cbede43b518c693fbd457af9a5e83d126
Chris Hagedorn <1844858+ChrisHagedorn@users.noreply.github.com>
2023-08-07 12:28:51 +0800

    generate quant lib

--- main.py
+++ main.py
@@ -1,47 +1,23 @@
+import json
 import pandas as pd
-import requests
-import yfinance as yf
-from bs4 import BeautifulSoup
-import xlsxwriter
 
-def get_sp500_instruments():
-    res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-    soup = BeautifulSoup(res.content,'lxml')
-    table = soup.find_all('table')[0] 
-    df = pd.read_html(str(table))
-    return list(df[0]["Symbol"])
+from dateutil.relativedelta import relativedelta
 
+import quantlib.data_utils as du
+import quantlib.general_utils as gu
 
-def get_sp500_df():
-    symbols = get_sp500_instruments()
-    symbols = symbols[:30] 
-    ohlcvs = {}
-    for symbol in symbols:
-        symbol_df = yf.Ticker(symbol).history(period="10y")
-        ohlcvs[symbol] = symbol_df[["Open", "High", "Low", "Close", "Volume"]].rename(
-            columns={
-                "Open": "open",
-                "High": "high",
-                "Low": "low",
-                "Close": "close",
-                "Volume": "volume"
-            }
-        )
-        print(symbol)
-        print(ohlcvs[symbol])
-    
-    df = pd.DataFrame(index=ohlcvs["GOOGL"].index)
-    df.index = df.index.tz_localize(None)
-    df.index.name = "date"
-    instruments = list(ohlcvs.keys())
-    print(instruments)
-    for inst in instruments:
-        inst_df = ohlcvs[inst]
-        columns = list(map(lambda x: "{} {}".format(inst, x), inst_df.columns))
-        df[columns] = inst_df
+from subsystems.LBMOM.subsys import Lbmom
 
-    return df, instruments
+df, instruments = gu.load_file("./Data/data.obj")
+print(df, instruments)
 
-df, instruments = get_sp500_df()
+#run simulation for 5 years
+VOL_TARGET = 0.20
+print(df.index[-1]) #date today: 2022-02-04
+sim_start = df.index[-1] - relativedelta(years=5)
+print(sim_start) #start trading backtest from 017-02-04
 
-# df.to_excel("./data/sp500_data.xlsx", engine='xlsxwriter')  
+strat = Lbmom(instruments_config="./subsystems/LBMOM/config.json", historical_df=df, simulation_start=sim_start, vol_target=VOL_TARGET)
+strat.get_subsys_pos()
+
+#we managed to successfully integrate this to the main driver, and can access the Lbmom strategy from our main script.
